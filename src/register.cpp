e62545e7c9847872a4b0dddfdab81c52c65acce4
caianto <105373685+miraloqueseavecinaalavuetadelaesquina@users.noreply.github.com>
2022-10-04 22:13:06 -0300

    registro Medio en register.cpp

--- src/register.cpp
+++ src/register.cpp
@@ -5,6 +5,7 @@
 
 void register_atomics_on(ParallelModelAdmin &admin)
 {
+	admin.registerAtomic(NewAtomicFunction< Medio >(), ATOMIC_MODEL_NAME);
 	admin.registerAtomic(NewAtomicFunction< Persona >(), ATOMIC_MODEL_NAME); // Register custom models
 }
 
